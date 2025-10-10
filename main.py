d0168e5fb293ac1f687013e5d83ad0d13db95a8e
RAMANAMBONONA <105931833+ramanambonona@users.noreply.github.com>
2025-10-10 12:00:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os, sys
+ROOT = os.path.dirname(os.path.abspath(__file__))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
 import streamlit as st
 from modules.accueil import app as accueil
 from modules.secteur_reel import app as secteur_reel
@@ -5,7 +10,12 @@ from modules.finances_publiques import app as finances_publiques
 from modules.secteur_monetaire import app as secteur_monetaire
 from modules.secteur_exterieur import app as secteur_exterieur
 
-st.set_page_config(page_title="Économie de Madagascar", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+    page_title="Économie de Madagascar",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 MENU = {
     "🏠 Accueil": accueil,
@@ -21,4 +31,5 @@ with st.sidebar:
         st.session_state["nav_choice"] = "🏠 Accueil"
     choix = st.radio("Navigation", list(MENU.keys()), key="nav_choice")
 
+# Exécuter la page choisie
 MENU[choix]()
